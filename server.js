3da52e6f363703524e8c01b4d8d865db39db15b1
Walison Brandão <walisonbrandao30@gmail.com>
2025-04-23 19:22:40 -0300

    Finalizado

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const dentalRouter = require('./src/routes/dentalRecord.route.js')
 const generalRecord = require('./src/routes/gR.route.js')
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 const app = express();
 
 app.use(cors());
