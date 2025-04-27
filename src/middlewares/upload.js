1d13ee4162bf66516ef12e706eca589939bfdb3b
Walison Brandão <walisonbrandao30@gmail.com>
2025-04-26 21:46:17 -0300

    Finalizado

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -5,17 +5,23 @@ const { storage } = require('../utils/cloudinary');
 const upload = multer({
   storage: storage,
   fileFilter: function (req, file, cb) {
-    const allowedTypes = /jpeg|jpg|png|gif|mp4|pdf/;
-    const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = allowedTypes.test(file.mimetype);
-
+    const allowedTypes = /jpeg|jpg|png|gif|mp4|pdf/
+    
+    // Verifica a extensão do arquivo
+    const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase())
+    
+    // Verifica o mimetype do arquivo
+    const mimetype = allowedTypes.test(file.mimetype)
+    
+    // Se ambos forem verdadeiros (extensão e mimetype), aceita o arquivo
     if (extname && mimetype) {
-      return cb(null, true);
+      return cb(null, true)
     } else {
-      cb(new Error('Arquivo inválido! Apenas imagens, vídeos e PDFs são permitidos.'));
+      // Caso contrário, retorna erro
+      cb(new Error('Arquivo inválido! Apenas imagens, vídeos e PDFs são permitidos'))
     }
   },
-  limits: { fileSize: 50 * 1024 * 1024 } // 50MB
-});
+  limits: { fileSize: 50 * 1024 * 1024 } // Limite de 50MB
+})
 
 module.exports = upload;
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,19 +6,19 @@ cloudinary.config()
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: async (req, file) => {
-        let resourceType = 'image'
-
-        if (file.mimetype.startsWith('video/')) {
-            resourceType = 'video'
-        } else if (file.mimetype === 'application/pdf') {
-            resourceType = 'raw'
-
-        }
-        return{
-            folder: 'stpericial',
-            allowed_format: ['jpg', 'jpeg', 'png', 'gif', 'mp4', 'pdf'],
-            resource_type: resourceType,
-        } 
+      let resourceType = 'image';
+  
+      if (file.mimetype.startsWith('video/')) {
+        resourceType = 'video'
+      } else if (file.mimetype === 'application/pdf') {
+        resourceType = 'raw'
+      }
+  
+      return {
+        folder: 'stpericial',
+        allowed_formats: ['jpg', 'jpeg', 'png', 'gif', 'mp4', 'pdf'],
+        resource_type: resourceType,
+      }
     }
 })
 
